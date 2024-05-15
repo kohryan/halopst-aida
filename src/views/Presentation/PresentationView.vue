18c3073f9d50da7a52b3e819ebc0fa955cfc5eab
Ryan W. Januardi <rw.januardi@gmail.com>
2024-05-15 07:36:11 +0700

    add design

---
+++ src/views/Presentation/PresentationView.vue
@@ -0,0 +1,230 @@
+<script setup>
+import { onMounted, onUnmounted } from "vue";
+
+//example components
+import NavbarDefault from "../../examples/navbars/NavbarDefault.vue";
+import DefaultFooter from "../../examples/footers/FooterDefault.vue";
+import Header from "../../examples/Header.vue";
+import FilledInfoCard from "../../examples/cards/infoCards/FilledInfoCard.vue";
+
+//Vue Material Kit 2 components
+import MaterialSocialButton from "@/components/MaterialSocialButton.vue";
+
+// sections
+import PresentationPages from "./Sections/PresentationPages.vue";
+import BuiltByDevelopers from "./Components/BuiltByDevelopers.vue";
+import PresentationTestimonials from "./Sections/PresentationTestimonials.vue";
+import PresentationInformation from "./Sections/PresentationInformation.vue";
+
+//images
+import bg0 from "@/assets/img/bg9.jpg";
+import wavesWhite from "@/assets/img/waves-white.svg";
+import logoBootstrap from "@/assets/img/logos/bootstrap5.jpg";
+import logoTailwind from "@/assets/img/logos/icon-tailwind.jpg";
+import logoVue from "@/assets/img/logos/vue.jpg";
+import logoAngular from "@/assets/img/logos/angular.jpg";
+import logoReact from "@/assets/img/logos/react.jpg";
+import logoSketch from "@/assets/img/logos/sketch.jpg";
+
+//dep
+import Typed from "typed.js";
+
+//hooks
+const body = document.getElementsByTagName("body")[0];
+onMounted(() => {
+  body.classList.add("presentation-page");
+  body.classList.add("bg-gray-200");
+
+  if (document.getElementById("typed")) {
+    // eslint-disable-next-line no-unused-vars
+    var typed = new Typed("#typed", {
+      stringsElement: "#typed-strings",
+      typeSpeed: 90,
+      backSpeed: 90,
+      backDelay: 200,
+      startDelay: 500,
+      loop: true,
+    });
+  }
+  
+});
+onUnmounted(() => {
+  body.classList.remove("presentation-page");
+  body.classList.remove("bg-gray-200");
+});
+
+</script>
+
+<template>
+  <div class="container position-sticky z-index-sticky top-0">
+    <div class="row">
+      <div class="col-12">
+        <NavbarDefault :sticky="true" />
+      </div>
+    </div>
+  </div>
+  <Header>
+    <div
+      class="page-header min-vh-75"
+      :style="`background-image: url(${bg0})`"
+      loading="lazy"
+    >
+      <span class="mask bg-gradient-dark opacity-2"></span>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-7 text-center mx-auto position-relative">
+            <h1
+              class="text-white pt-3 mt-n5 me-2"
+              :style="{ display: 'inline-block ' }"
+            >
+            Pelayanan Statistik 24 Jam
+            </h1>
+            <p class="text-white px-5 mt-1" :style="{ fontWeight: '500' }">
+              Pencarian Data dan Konsultasi Statistik yang lebih <span class="text-white" id="typed"></span>
+            </p>
+            <div id="typed-strings">
+              <h1>mudah</h1>
+              <h1>cepat</h1>
+              <h1>fleksibel</h1>
+            </div>
+          </div>
+          <div class="col-lg-6 mx-auto position-relative">
+            <div class="input-group mt-3">
+              <input type="text" class="form-control bg-white px-3" placeholder="Cari Data Apa?" aria-label="Recipient's username" aria-describedby="button-addon2" v-model="searchQuery" @keyup.enter="search">
+              <button class="btn btn-success" style="margin-bottom: 0;" type="button-lg" id="button-addon2" @click="search">Cari</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </Header>
+
+  <div class="card card-body blur shadow-blur mx-3 mx-md-4 mt-n6">
+    <PresentationInformation />
+    <PresentationPages />
+    <BuiltByDevelopers />
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4">
+          <FilledInfoCard
+            class="p-4"
+            :color="{ text: 'white', background: 'bg-gradient-success' }"
+            :icon="{ component: 'flag', color: 'white' }"
+            title="Getting Started"
+            description="Check the possible ways of working with our product and the necessary files for building your own project."
+            :action="{
+              route:
+                'https://www.creative-tim.com/learning-lab/vue/overview/material-kit/',
+              label: { text: 'Let\'s start', color: 'white' }
+            }"
+          />
+        </div>
+        <div class="col-lg-4">
+          <FilledInfoCard
+            class="px-lg-1 mt-lg-0 mt-4 p-4"
+            height="h-100"
+            :icon="{ component: 'precision_manufacturing', color: 'success' }"
+            title="Plugins"
+            description="Get inspiration and have an overview about the plugins that we
+                used to create the Material Kit."
+            :action="{
+              route:
+                'https://www.creative-tim.com/learning-lab/vue/input/material-kit/',
+              label: { text: 'Read more' }
+            }"
+          />
+        </div>
+        <div class="col-lg-4">
+          <FilledInfoCard
+            class="px-lg-1 mt-lg-0 mt-4 p-4"
+            :icon="{ component: 'receipt_long', color: 'success' }"
+            title="Utility Classes"
+            description="Material Kit is giving you a lot of pre-made elements. For those
+                who want flexibility, we included many utility classes."
+            :action="{
+              route:
+                'https://www.creative-tim.com/learning-lab/vue/utilities/material-kit/',
+              label: { text: 'Read more' }
+            }"
+          />
+        </div>
+      </div>
+    </div>
+    <PresentationTestimonials />
+
+    <div
+      class="container-fluid mt-sm-5 border-radius-xl"
+      :style="{
+        background: 'linear-gradient(195deg, rgb(66, 66, 74), rgb(25, 25, 25))'
+      }"
+    >
+      <div
+        class="page-header py-6 py-md-5 my-sm-3 mb-3 border-radius-xl"
+        :style="{
+          backgroundImage: `url(${wavesWhite})`
+        }"
+        loading="lazy"
+      >
+        <span class="mask bg-gradient-dark"></span>
+        <div class="container">
+          <div class="row">
+            <div class="d-flex justify-content-center p-5">
+              <div class="col-lg-8 ms-lg-5 text-center">
+                <h3 class="text-white">
+                  Do you love this awesome UI Kit from Vuejs & Bootstrap?
+                </h3>
+                <p class="text-white text-md">
+                  Cause if you do, it can be yours for FREE. Hit the button
+                  below to navigate to Creative Tim where you can <br />
+                  find the Design System in HTML. Start a new project or give an
+                  old Bootstrap project a new look!
+                </p>
+
+                <a
+                  href="https://www.creative-tim.com/product/vue-material-kit"
+                  class="btn btn-sm mb-0 bg-gradient-success px-5 py-3 mt-4"
+                  >Download Now</a
+                >
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="py-5">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-5 ms-auto">
+            <h5>Badan Pusat Statistik</h5>
+            <h5 class="mb-1">Provinsi Jawa Timur</h5>
+            <p class="mb-0">We deliver the best web products</p>
+          </div>
+          <div class="col-lg-5 me-lg-auto my-lg-auto text-lg-end mt-5">
+            <MaterialSocialButton
+              route="https://twitter.com/intent/tweet?text=Check%20Material%20Design%20System%20made%20by%20%40CreativeTim%20%23webdesign%20%23designsystem%20%23bootstrap5&url=https%3A%2F%2Fwww.creative-tim.com%2Fproduct%2Fmaterial-design-system-pro"
+              component="twitter"
+              color="twitter"
+              label="Tweet"
+            />
+            <MaterialSocialButton
+              route="https://www.facebook.com/sharer/sharer.php?u=https://www.creative-tim.com/product/material-design-system-pro"
+              component="facebook-square"
+              color="facebook"
+              label="Share"
+            />
+            <MaterialSocialButton
+              route=""
+              component="pinterest"
+              color="pinterest"
+              label="Pin it"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <DefaultFooter />
+</template>
+
---
+++ src/views/Presentation/Sections/PresentationInformation.vue
@@ -0,0 +1,66 @@
+<script setup>
+import RotatingCard from "../../../examples/cards/rotatingCards/RotatingCard.vue";
+import RotatingCardFront from "../../../examples/cards/rotatingCards/RotatingCardFront.vue";
+import RotatingCardBack from "../../../examples/cards/rotatingCards/RotatingCardBack.vue";
+</script>
+<template>
+  <section class="my-2 py-2">
+    <div class="container">
+      <div class="row justify-content-center text-center my-sm-3">
+          <div class="col-lg-6">
+            <h3 class="text-dark mb-0">Masih bingung dengan data BPS?</h3>
+          </div>
+        </div>
+      <div class="row align-items-center">
+        <div class="col-md-10 mx-auto">
+          <div class="row">
+            <div class="col-lg-5 mx-auto p-lg-4 mt-lg-0 mt-2">
+              <RotatingCard>
+              <RotatingCardFront
+              image="https://images.unsplash.com/photo-1569683795645-b62e50fbf103?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80"
+              icon="touch_app"
+              title="Konsultasi dengan<br /> Statistisi Ahli BPS"
+              description="BPS Jawa Timur memiliki ratusan Statistisi Ahli dan Analis Data berkompeten yang siap membantu Anda"
+              />
+
+              <RotatingCardBack
+              image="https://images.unsplash.com/photo-1498889444388-e67ea62c464b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1365&q=80"
+              title="Buat Reservasi"
+              description="Klik tombol di bawah ini untuk membuat reservasi jadwal konsultasi sesuai dengan kebutuhan Anda"
+              :action="[
+                {
+                  route: '/konsultasi',
+                  label: 'Buat Reservasi',
+                },
+              ]"
+              />
+              </RotatingCard>
+            </div>
+            <div class="col-lg-5 ms-auto me-auto p-lg-4 mt-lg-0 mt-2">
+              <RotatingCard>
+              <RotatingCardFront
+              image="https://images.unsplash.com/photo-1569683795645-b62e50fbf103?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80"
+              icon="touch_app"
+              title="Konsultasi dengan<br /> Statistisi Ahli BPS"
+              description="BPS Jawa Timur memiliki ratusan Statistisi Ahli dan Analis Data berkompeten yang siap membantu Anda"
+              />
+
+              <RotatingCardBack
+              image="https://images.unsplash.com/photo-1498889444388-e67ea62c464b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1365&q=80"
+              title="Buat Reservasi"
+              description="Klik tombol di bawah ini untuk membuat reservasi jadwal konsultasi sesuai dengan kebutuhan Anda"
+              :action="[
+                {
+                  route: '/konsultasi',
+                  label: 'Buat Reservasi',
+                },
+              ]"
+              />
+              </RotatingCard>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
